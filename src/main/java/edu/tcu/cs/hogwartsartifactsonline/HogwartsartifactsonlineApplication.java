26f19f9cc47ac43867e1fcacb82c864cf36435b4
washingtonwei <b.wei@tcu.edu>
2020-04-21 22:30:59 -0500

    Initial commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsartifactsonlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsartifactsonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsartifactsonlineApplication.class, args);
+    }
+
+}
